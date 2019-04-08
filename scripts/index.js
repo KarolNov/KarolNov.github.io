165bd212377b0222be2b7ab9837fd868153455a7
KarolNov <karolnovljakovic@gmail.com>
2019-04-08 17:58:36 +0200

    started filling in sections. Section - skills.

--- scripts/index.js
+++ scripts/index.js
@@ -1,60 +1,61 @@
 import translation from './translation';
 
-window.onload = ()=>{
+window.onload = () => {
     langSwitch();
     welcomeAnimation();
-    setTimeout(()=>{
+    setTimeout(() => {
         showMenu();
     }, 5200);
+    handleModals();
 }
 
-const langSwitch = ()=>{
+const langSwitch = () => {
     let switcher = document.getElementById("langSwitch");
     var language = window.navigator.userLanguage || window.navigator.language;
     document.documentElement.lang = language;
-    if(language==="pl"){
+    if (language === "pl") {
         switcher.innerHTML = "<p><b>PL</b>|EN</p>";
         translation.translate("pl");
-    }else{
+    } else {
         switcher.innerHTML = "<p>PL|<b>EN</b></p>";
         translation.translate("en");
     }
 
-    switcher.onclick = ()=>{
-        if(language==="pl"){
-            language="en";
-        }else{
-            language="pl";
+    switcher.onclick = () => {
+        if (language === "pl") {
+            language = "en";
+        } else {
+            language = "pl";
         }
         document.documentElement.lang = language;
-        if(language==="pl"){
+        if (language === "pl") {
             switcher.innerHTML = "<p><b>PL</b>|EN</p>";
             translation.translate("pl");
-        }else{
+        } else {
             switcher.innerHTML = "<p>PL|<b>EN</b></p>";
             translation.translate("en");
         }
     }
 }
 
-const welcomeAnimation = ()=>{
+const welcomeAnimation = () => {
     let slideFirst = document.getElementById('underline');
     slideFirst.style.width = "100%";
-    setTimeout(()=>{
+    setTimeout(() => {
         slideFirst.style.height = "calc(100%)"
     }, 3100)
 }
 
-const showMenu = ()=>{
+const showMenu = () => {
     //order h1 and it's content elements so they are not stacked one on another
     let menu = document.getElementById('menu');
     menu.style.opacity = 1;
     let navs = document.querySelectorAll('#menu .link');
-    navs.forEach((h,i)=>{
-        h.onclick = ()=>{
+    navs.forEach((h, i) => {
+        h.onclick = () => {
             //slide menu to the top
-            if(i!==0){
-                h.parentElement.parentElement.style.marginTop = -i*8+"rem";
+            if (i !== 0) {
+                h.parentElement.parentElement.style.marginTop = -i * 8 + "rem";
             }
             //slide this link to the left and center
             h.parentElement.style.left = "50%";
@@ -64,25 +65,38 @@ const showMenu = ()=>{
             let link = h.firstChild.href.split("#")[1].concat("_content");
             let content = document.getElementById(link);
             content.style.display = "block";
-            setTimeout(()=>{
-                content.style.height = "100vh";
+            let titleHeight = document.getElementById("welcomeAnimation").offsetHeight;
+            setTimeout(() => {
+                content.style.height = `calc(100vh - 7.5rem - ${titleHeight}px)`;
             }, 10);
             //back button function
             let back = document.getElementById('back');
-            setTimeout(()=>{
+            setTimeout(() => {
                 back.style.opacity = 1;
             }, 1000);
-            back.onclick = ()=>{
+            back.onclick = () => {
                 h.parentElement.parentElement.style.marginTop = 0;
                 h.parentElement.style.left = "5px";
                 h.parentElement.style.transform = "";
                 content.style.height = 0;
-                setTimeout(()=>{
+                setTimeout(() => {
                     content.style.display = "none";
                 }, 2000);
                 back.style.opacity = 0;
             }
         }
     })
-    
+}
+
+const handleModals = ()=>{
+    let toggles = Array.from(document.getElementsByClassName("toggleModal"));
+
+    toggles.forEach(el=>{
+        el.onclick = (e)=>{
+            let target = el.attributes["data-modal-target"].value;
+            let modal = document.getElementById(target);
+            modal.style.display = modal.style.display==="block" ? "none" : "block";
+        }
+    })
+
 }
--- scripts/translation.js
+++ scripts/translation.js
@@ -20,6 +20,77 @@ const translation = {
         en: "my skills",
         pl: "moje umiejętności"
     },
+    "skills_title_dev": {
+        en: "Developer skills <br/> <em>Press icons to read more</em>",
+        pl: "Umiejętności programistyczne <br/> <em>Naciśnij ikonę, aby przeczytać więcej</em>"
+    },
+    "js_title": {
+        en: "My Javascript skills:",
+        pl: "Moje umiejętności w Javascript:"
+    },
+    "js_list": {
+        en: 
+        `
+            <li>
+        `,
+        pl:
+        `
+            <li> Umiejętnie posługuję się najnowszą wersją ECMAScript (nawet do ES2019) </li>
+            <li> Potrafię korzystać z dokumentacji (m.in. MDN) </li>
+            <li> Staram się utrzymywać czysty kod, korzystam z JSDoc3 do opisu i komentarzy</li>
+        `
+    },
+    "skills_title_soft": {
+        en: "Soft skills",
+        pl: "Umiejętności miękkie"
+    },
+    "soft_skill_org": {
+        en: `
+        <h6>Organization and project managment</h6>
+        <p>
+            I got 3 years of experience in project and team managment (as a team leader as well as active member) in the biggest polish NGO. <br/>
+            As scout instructor I learned to manage both project and permanent teams. I know and use planning and situation analysis techniques. 
+            <br/>
+            See example: <br/><a href="./data/plan.pdf" target="_blank">strategic plan of Scout Group I created</a>.
+        </p>`,
+        pl: `
+        <h6>Organizacja i zarządzanie projektami</h6>
+        <p>
+            Mam 3 letnie doświadczenie w zarządzaniu projektami i zespołami (zarówno jako ich aktywny członek jak i lider) w największym polskim NGO.
+            <br/>
+            Jako instruktor harcerski nauczyłem się zarządzać zarówno zespołami projektowymi, jak i ciągłymi. Znam i korzystam z technik analizy sytuacji i planowania. 
+            <br/>
+            Zobacz na przykładzie: <br/><a href="./data/plan.pdf" target="_blank">plan strategiczny Szczepu, który stworzyłem</a>.
+        </p>`
+    },
+    "soft_skill_cs": {
+        en: `
+        <h6>Customer service</h6>
+        <p>
+            I have yearly experience in work with clients as a employee of movie set equipment rental company. My responsibilites consisted of keeping in touch with clients (e.g. telephone duty), answering their needs, advising and paperwork - checking emails, managing reservations.
+            <br/>
+            Outside of office I had direct contact with client as an PA (movie set assistant).
+        </p>`,
+        pl: `
+        <h6>Obsługa klienta</h6>
+        <p>
+            Mam roczne doświadczenie w pracy z klientami jako pracownik wypożyczalni sprzętu filmowego. Moją odpowiedzialnością było pozostawanie z klientami w ciągłym kontakcie (np. w formie dyżurów telefonicznych), odpowiadanie na ich potrzeby, doradzanie oraz kwestie biurowe - odpisywanie na emaile, zarządzanie rezerwacjami. 
+            <br/>
+            Poza pracą biurową miałem też bezpośrednią styczność z klientem jako asystent na planach i sztukach.
+        </p>`
+    },
+    "soft_skill_q": {
+        en: `
+        <h6>Ability to ask the right questions</h6>
+        <p>
+        I'm not afraid to have doubts and present my opinion. I know how to read documentations and I always seek a solution to the problem before asking the question to someone with more knowledge.
+        </p>`,
+        pl: `
+        <h6>Umiejętność zadawania właściwych pytań</h6>
+        <p>
+        Nie boję się mieć wątpliwości i przedstawiać swoją opinię. Umiem czytać dokumentację i zawsze szukam rozwiązania problemu zanim zadam pytanie komuś z większą wiedzą.
+        </p>`
+    },
     "portfolio_link": {
         en: "my works",
         pl: "moje prace"
